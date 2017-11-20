3e85785bdcbc9abd29e4204365941a2285d1b7bf
Roy Dammarell <roy.dammarell@gmail.com>
2017-11-19 17:20:18 -0800

    Remove empty constructor

--- XamarinDependency.Library/HelloXamarinService.cs
+++ XamarinDependency.Library/HelloXamarinService.cs
@@ -7,10 +7,6 @@ namespace XamarinDependency.Library
     [Register("com/roydammarell/xamarindependency/HelloXamarinService")]
     public class HelloXamarinService : Java.Lang.Object
     {
-        public HelloXamarinService()
-        {
-        }
-
         [Export]
         public string CreateHelloXamarin()
         {
