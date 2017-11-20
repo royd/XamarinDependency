355e9fa45329fabd37d724a2f0090e9bf972d579
Roy Dammarell <roy.dammarell@gmail.com>
2017-11-19 17:20:43 -0800

    Add exception service

---
+++ XamarinDependency.Library/ExceptionXamarinService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.Runtime;
+using Java.Interop;
+
+namespace XamarinDependency.Library
+{
+    [Register("com/roydammarell/xamarindependency/ExceptionXamarinService")]
+    public class ExceptionXamarinService : Java.Lang.Object
+    {
+        [Export]
+        public void ThrowNullReferenceException()
+        {
+            throw new NullReferenceException("Testing C# NullReferenceException");
+        }
+    }
+}
