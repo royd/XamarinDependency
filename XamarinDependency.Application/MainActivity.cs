7e52602c7761a3979151a55da1acc6b51c06ffd1
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2020-07-14 20:17:09 -0400

    Migrated project to AndroidX

--- XamarinDependency.Application/MainActivity.cs
+++ XamarinDependency.Application/MainActivity.cs
@@ -1,27 +1,17 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using AndroidX.AppCompat.App;
 
 namespace XamarinDependency.Application
 {
-    [Activity(Label = "XamarinDependency", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
-
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            SetContentView(Resource.Layout.activity_main);
         }
     }
 }
-
